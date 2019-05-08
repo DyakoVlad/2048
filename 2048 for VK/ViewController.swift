46cb95a87011e0e82b1f9b14d08e7688bdfeab80
Dyakov Vlad <dyakov-vladislav@yandex.ru>
2019-05-08 17:25:53 +0300

    Initial Commit

---
+++ 2048 for VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2048 for VK
+//
+//  Created by Ирина Дьякова on 08/05/2019.
+//  Copyright © 2019 Vlad Dyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
